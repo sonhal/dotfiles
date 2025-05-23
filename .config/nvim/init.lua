bd1d63f8c92db9c9ad4e790f2062cfe06f9e71a8
Sondre <sondre@sonhal.no>
2025-05-23 16:53:04 +0000

    add tmux navigator plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -997,6 +997,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   { import = 'custom.plugins' },
+  { import = 'plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
