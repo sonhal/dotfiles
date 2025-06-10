94461451bb2bd516ffaf19d2f52d1f85c1ea833e
Sondre <sondre@sonhal.no>
2025-06-10 18:00:48 +0000

    Add test and tmux navigation plugins

---
+++ .config/nvim/lua/plugins/nvim-neotest.lua
@@ -0,0 +1,105 @@
+return {
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-neotest/neotest-python',
+      'nvim-neotest/neotest-vim-test',
+      'nvim-treesitter/nvim-treesitter',
+      {
+        'fredrikaverpil/neotest-golang', -- Installation
+        dependencies = {
+          'leoluz/nvim-dap-go',
+        },
+      },
+    },
+    keys = {
+      { '<leader>t', '', desc = '+test' },
+      {
+        '<leader>tt',
+        function()
+          require('neotest').run.run(vim.fn.expand '%')
+        end,
+        desc = 'Run File (Neotest)',
+      },
+      {
+        '<leader>tT',
+        function()
+          require('neotest').run.run(vim.uv.cwd())
+        end,
+        desc = 'Run All Test Files (Neotest)',
+      },
+      {
+        '<leader>tr',
+        function()
+          require('neotest').run.run()
+        end,
+        desc = 'Run Nearest (Neotest)',
+      },
+      {
+        '<leader>tl',
+        function()
+          require('neotest').run.run_last()
+        end,
+        desc = 'Run Last (Neotest)',
+      },
+      {
+        '<leader>ts',
+        function()
+          require('neotest').summary.toggle()
+        end,
+        desc = 'Toggle Summary (Neotest)',
+      },
+      {
+        '<leader>to',
+        function()
+          require('neotest').output.open { enter = true, auto_close = true }
+        end,
+        desc = 'Show Output (Neotest)',
+      },
+      {
+        '<leader>tO',
+        function()
+          require('neotest').output_panel.toggle()
+        end,
+        desc = 'Toggle Output Panel (Neotest)',
+      },
+      {
+        '<leader>tS',
+        function()
+          require('neotest').run.stop()
+        end,
+        desc = 'Stop (Neotest)',
+      },
+      {
+        '<leader>tw',
+        function()
+          require('neotest').watch.toggle(vim.fn.expand '%')
+        end,
+        desc = 'Toggle Watch (Neotest)',
+      },
+      {
+        '<leader>td',
+        function()
+          require('neotest').run.run { suite = false, strategy = 'dap' }
+        end,
+        desc = 'Debug nearest test',
+      },
+    },
+    opts = function()
+      return {
+        adapters = {
+          require 'neotest-python' {
+            dap = { justMyCode = false },
+          },
+          require 'neotest-vim-test' {
+            ignore_file_types = { 'python', 'vim', 'lua' },
+          },
+          require 'neotest-golang',
+        },
+      }
+    end,
+  },
+}
