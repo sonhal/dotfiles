973f79d55e7eac96ea944105ba6e32c15dec7423
Name <user@example.com>
2025-11-17 15:51:58 +0100

    Add lsp and module for java

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -667,7 +667,9 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        jdtls = {},
         vue_ls = {},
+        vtsls = {},
         clangd = {},
         gopls = {},
         -- pylsp = {
