0e9150c0ca3e853d68a8f576389f2c56f07353c2
Sondre <sondre@sonhal.no>
2026-09-15 22:23:49 +0200

    convert hyprland config to new lua format

---
+++ .config/hypr/envs.lua
@@ -0,0 +1,43 @@
+-- Cursor size
+hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("XCURSOR_THEME", "Adwaita")
+
+-- Force all apps to use Wayland
+hl.env("GDK_BACKEND", "wayland,x11,*")
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+hl.env("SDL_VIDEODRIVER", "wayland")
+hl.env("MOZ_ENABLE_WAYLAND", "1")
+hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
+hl.env("OZONE_PLATFORM", "wayland")
+hl.env("XDG_SESSION_TYPE", "wayland")
+
+-- Allow better support for screen sharing (Google Meet, Discord, etc)
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+
+-- Dark mode
+hl.env("GTK_THEME", "adw-gtk3-dark")
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("QT_STYLE_OVERRIDE", "kvantum")
+hl.env("GTK_USE_PORTAL", "1")
+
+-- Use XCompose file
+hl.env("XCOMPOSEFILE", os.getenv("HOME") .. "/.XCompose")
+
+hl.config({
+    xwayland = {
+        force_zero_scaling = true,
+    },
+    ecosystem = {
+        -- Don't show update news on first launch
+        no_update_news = true,
+    },
+})
+
+-- Dark mode gsettings
+hl.on("hyprland.start", function()
+    hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'")
+    hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
+    hl.exec_cmd("gsettings set org.gnome.desktop.interface icon-theme 'Adwaita'")
+end)
