b90dbb79d07a8e01f50a2b9b0cc6635c1c9eafc2
Sondre <sondre@sonhal.no>
2026-09-20 19:51:11 +0200

    remove git ignore for lock file for dependencies

--- .config/nvim/lua/plugins/nvim-jdtls.lua
+++ .config/nvim/lua/plugins/nvim-jdtls.lua
@@ -1,3 +1,7 @@
+-- Java: jdtls is started by ftplugin/java.lua via nvim-jdtls, not by lspconfig.
+-- nvim-jdtls adds organize imports, extract refactorings, test running and debugging on top of plain jdtls.
 return {
   'mfussenegger/nvim-jdtls',
+  ft = 'java',
+  dependencies = { 'mfussenegger/nvim-dap' },
 }
